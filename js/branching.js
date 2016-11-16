1c955dff000ae41e068585f6bdd37dd592f2408e
Ben Walley <benwalleyorigami@gmail.com>
2016-11-16 11:37:36 -0600

    testing

--- js/branching.js
+++ js/branching.js
@@ -3,6 +3,7 @@ var width = 50
 var height = 0
 var first = false
 var sections = 1
+var sec = 1
 
 
 
@@ -35,30 +36,44 @@ $(window).scroll(function(){
 				height += 10
 			}else{
 				// then on to the next row
-				row = 1	
+				row = 1
+				first = true	
 			}
 		}		
 	}
 	// IF IT IS NOT THE FIRST ROW
 	else{
-		for(var i = 0; i < sections; i++){
-			// MAKE A DIV FOR EACH DOUBLE BRANCH
-			var $row = $("<div>", {"class": "row", width: 100/sections + "%", height: "20px"});
-			$("#main").append($row);
-			// INSIDE EACH ONE, MAKE A DIV WITH TWO SIDE DIVS
-			var $left = $("<div>", { "class": "side", width: "25%", height: "100%"});
-			$left.appendTo($(".row")[i]);
+		if(first){
+			for(var i = 0; i < sections; i++){
+				// MAKE A DIV FOR EACH DOUBLE BRANCH
+				var $row = $("<div>", {"class": "row", width: 100/sections + "%", height: "10px"});
+				$("#main").append($row);
+				// INSIDE EACH ONE, MAKE A DIV WITH TWO SIDE DIVS
+				var $left = $("<div>", { "class": "side", width: "45%", height: "100%"});
+				$left.appendTo($(".row")[i]);
 
-			var $middle = $("<div>", { "class": "middle", width: "50%", height: "100%"});
-			$middle.appendTo($(".row")[i]);
+				var $middle = $("<div>", { "class": "middle", width: "10%", height: "100%"});
+				$middle.appendTo($(".row")[i]);
 
-			var $right = $("<div>", { "class": "side", width: "25%", height: "100%"});
-			$right.appendTo($(".row")[i]);
-			console.log("made stuff")
-		}		
+				var $right = $("<div>", { "class": "side", width: "45%", height: "100%"});
+				$right.appendTo($(".row")[i]);
+
+				
+			}	
+			sections *= 2
+		}else{
+
+			// window.requestAnimationFrame(slideWide)
+		}
+			
 	}	
 })
 
-function slideWide(i){
+// function slideWide(){
+// 	for(var i = 0; i < sections.length; i++){
+// 		if(){
 
-}
+// 		}
+// 	}
+	
+// // }
